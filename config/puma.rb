6f895b02cebfe6736ad7160c519c2682496d34cb
George <ezen.1.618@gmail.com>
2021-04-08 14:57:31 -0400

    puma, Procfile

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,4 @@
+=begin
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -35,3 +36,16 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+=end
+
+# heroku setup --------------- 
+
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
